8125bcb82919e298f4c98b90cd18fa108a293405
nikitha26 <mnikitha365@gmail.com>
2022-01-31 16:25:26 +0530

    shopping cart

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.jpeg';
+const data ={
+    productData:[
+        {
+          id:1,
+          img:img1,
+          title:'Best Laptops',
+          desc:'',
+          price:1000
+        },
+        {
+            id:2,
+            img:img2,
+            title:'Best Laptops',
+            desc:'',
+            price:2000
+          },
+          {
+            id:3,
+            img:img3,
+            title:'Best Laptops',
+            desc:'',
+            price:3000
+          },
+    ]
+}
+
+export default data;
